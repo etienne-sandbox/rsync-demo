20e278d9404df4bac1c7d393af3d65c33d5290e4
Etienne Deladonchamps <e.deladonchamps@gmail.com>
2026-02-12 01:41:09 +0100

    Big cleanup

--- server/index.ts
+++ server/index.ts
@@ -1,31 +1,29 @@
-import { createServer, IncomingMessage } from "http";
-import { Buffer } from "buffer";
-import { readFile } from "fs/promises";
-import { diff } from "zen-rsync";
+import { diff } from "@dldc/librsync";
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { readFile } from "node:fs/promises";
 import prettyBytes from "pretty-bytes";
 
 const PORT = 3030;
 
-const server = createServer(async (req, res) => {
-  const checksum = await reqToBuffer(req);
-  console.log(`Server received checksum: ${prettyBytes(checksum.byteLength)}`);
-  const file = await readFile("./data/synced.txt");
-  console.log(`Server read file: ${prettyBytes(file.byteLength)}`);
-  const patch = diff(file, checksum);
-  console.log(`Server generated patch: ${prettyBytes(patch.byteLength)}`);
-  res.writeHead(200, { "Access-Control-Allow-Origin": "*" });
-  res.write(Buffer.from(patch));
-  res.end();
+const app = new Hono();
+
+app.use("*", cors());
+app.post("/", async (c) => {
+  try {
+    const checksum = new Uint8Array(await c.req.arrayBuffer());
+    console.log(`Server received checksum: ${checksum.byteLength} bytes`);
+    const file = await readFile("./data/synced.txt");
+    console.log(`Server read file: ${prettyBytes(file.byteLength)}`);
+    const patch = diff(checksum, file);
+    return c.body(patch, 200, { "Content-Type": "application/octet-stream" });
+  } catch (error) {
+    console.error(error);
+    return c.text("Internal Server Error", 500);
+  }
 });
 
-server.listen(PORT, () => {
+serve({ fetch: app.fetch, port: PORT }, () => {
   console.log(`Server running on port http://localhost:${PORT}`);
 });
-
-async function reqToBuffer(req: IncomingMessage): Promise<Buffer> {
-  const chunks = [];
-  for await (let chunk of req) {
-    chunks.push(chunk);
-  }
-  return Buffer.concat(chunks);
-}
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { Fragment, useCallback, useEffect, useRef, useState } from "react";
-import { apply, prepare } from "zen-rsync";
+import { apply, signature } from "@dldc/librsync";
 import prettyBytes from "pretty-bytes";
+import { Fragment, useCallback, useEffect, useRef, useState } from "react";
 
 export function App() {
-  const [file, setFile] = useState<ArrayBuffer>(() => new ArrayBuffer(0));
+  const [file, setFile] = useState<Uint8Array>(() => new Uint8Array(0));
 
   const [logs, setLogs] = useState<
     Array<{ time: Date; checksum: number; patch: number; file: number }>
@@ -26,8 +26,8 @@ export function App() {
 
   const sync = useCallback(async () => {
     const file = fileRef.current;
-    const blockSize = 512;
-    const checksum = prepare(file, blockSize);
+    const checksum = signature(file);
+    console.log(checksum);
     const controller = new AbortController();
     const signal = controller.signal;
     // log(`↑ Sending checksum`, checksum.byteLength);
@@ -39,7 +39,7 @@ export function App() {
       if (!response.ok) {
         throw new Error(response.statusText);
       }
-      const patch = await response.arrayBuffer();
+      const patch = new Uint8Array(await response.arrayBuffer());
       // log(`↓ Received patch`, patch.byteLength);
       if (controller.signal.aborted) {
         return;
@@ -79,7 +79,7 @@ export function App() {
           )}
         </pre>
       </div>
-      <div className="w-[400px] flex flex-col gap-4">
+      <div className="w-100 flex flex-col gap-4">
         <button
           className="rounded-md shadow-md bg-emerald-400 p-2 uppercase tracking-wider font-bold text-lg hover:bg-emerald-500"
           onClick={sync}
@@ -88,9 +88,7 @@ export function App() {
         </button>
         <button
           className={`rounded-md shadow-md p-2 uppercase tracking-wider font-bold text-lg ${
-            auto
-              ? `bg-emerald-400 hover:bg-emerald-500`
-              : `bg-gray-200 hover:bg-gray-300`
+            auto ? `bg-emerald-400 hover:bg-emerald-500` : `bg-gray-200 hover:bg-gray-300`
           }`}
           onClick={() => setAuto((auto) => !auto)}
         >
@@ -104,27 +102,24 @@ export function App() {
             Clear
           </button>
           <p className="mx-4 text-gray-700 flex flex-row justify-between">
-            <span className="text-gray-400 text-center w-20">Date</span>
-            <span className="text-gray-400 text-center w-20">Checksum</span>
-            <span className="text-gray-400 text-center w-20">Patch</span>
-            <span className="text-gray-400 text-center w-20">File</span>
+            <span className="text-gray-400 text-start w-20">Date</span>
+            <span className="text-gray-400 text-end w-20">Checksum</span>
+            <span className="text-gray-400 text-end w-20">Patch</span>
+            <span className="text-gray-400 text-end w-20">File</span>
           </p>
           <div className="px-4 flex flex-col gap-1 overflow-auto flex-1">
             {logs.map((log, index) => (
-              <p
-                key={index}
-                className="text-gray-700 flex flex-row justify-between"
-              >
+              <p key={index} className="text-gray-700 flex flex-row justify-between">
                 <span className="text-gray-400 inline-block w-20">
-                  {log.time.toLocaleTimeString()}
+                  {log.time.toLocaleTimeString("fr-FR")}
                 </span>
-                <span className="font-bold inline-block w-20 text-center">
+                <span className="font-bold inline-block w-20 text-end">
                   ↑ {prettyBytes(log.checksum)}
                 </span>
-                <span className="font-bold inline-block w-20 text-center">
+                <span className="font-bold inline-block w-20 text-end">
                   ↓ {prettyBytes(log.patch)}
                 </span>
-                <span className="font-bold inline-block w-20 text-center">
+                <span className="font-bold inline-block w-20 text-end">
                   {prettyBytes(log.file)}
                 </span>
               </p>
